fb210078e9506455e1087d20c747be3645e3f12c
Maria fernanda monroy <mafe2218monroy@gmail.com>
2024-11-19 19:55:00 -0500

    fixing endpoint error

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,11 @@ import { Routes } from '@angular/router';
 import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
 import { PokemonDetailComponent } from './components/pokemon-detail/pokemon-detail.component';
 
+// export const routes: Routes = [
+//   { path: '', component: PokemonListComponent },
+//   { path: ':name', component: PokemonDetailComponent },
 export const routes: Routes = [
-  { path: '', component: PokemonListComponent },
-  { path: ':name', component: PokemonDetailComponent },
+  { path: '', redirectTo: '/pokemons', pathMatch: 'full' },
+  { path: 'pokemons', component: PokemonListComponent },
+  { path: 'pokemon/:name', component: PokemonDetailComponent },
 ];
